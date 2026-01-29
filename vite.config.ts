29a15c4142723d327a95da89fe91a437a353de16
Adeoye John Oluwakayode <johnfixit293@gmail.com>
2026-01-29 11:53:45 +0100

    feat:added pwa feature

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,37 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    VitePWA({
+      registerType: "autoUpdate",
+      includeAssets: ["favicon.ico", "apple-touch-icon.png", "masked-icon.svg"],
+      manifest: {
+        name: "Logicrow",
+        short_name: "Logicrow App",
+        description: "Secure Logistics for Modern Business",
+        theme_color: "#ffffff",
+        background_color: "#ffffff",
+        display: "standalone",
+        icons: [
+          {
+            src: "logicrow.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "logicrow.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+        ],
+      },
+    }),
+  ],
 });
