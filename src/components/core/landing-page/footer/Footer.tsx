e391676b9b9734eebb861d09a3100d0734a07f50
Adeoye John Oluwakayode <johnfixit293@gmail.com>
2026-01-29 12:05:32 +0100

    feat:updted the app logo

--- src/components/core/landing-page/footer/Footer.tsx
+++ src/components/core/landing-page/footer/Footer.tsx
@@ -7,20 +7,14 @@ const Footer = () => {
             <div className="col-span-1 md:col-span-1 flex flex-col gap-6">
               <div className="flex items-center gap-3">
                 <div className="text-primary">
-                  <svg
-                    className="size-6"
-                    fill="none"
-                    viewBox="0 0 48 48"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      d="M42.1739 20.1739L27.8261 5.82609C29.1366 7.13663 28.3989 10.1876 26.2002 13.7654C24.8538 15.9564 22.9595 18.3449 20.6522 20.6522C18.3449 22.9595 15.9564 24.8538 13.7654 26.2002C10.1876 28.3989 7.13663 29.1366 5.82609 27.8261L20.1739 42.1739C21.4845 43.4845 24.5355 42.7467 28.1133 40.548C30.3042 39.2016 32.6927 37.3073 35 35C37.3073 32.6927 39.2016 30.3042 40.548 28.1133C42.7467 24.5355 43.4845 21.4845 42.1739 20.1739Z"
-                      fill="currentColor"
-                    ></path>
-                  </svg>
+                  <img
+                    src="/logicrow.png"
+                    alt=""
+                    className="objectcover h-12.5 w-12.5"
+                  />
                 </div>
                 <h2 className="text-lg font-bold leading-tight tracking-tight dark:text-white">
-                  LogisEscrow
+                  Logicrow
                 </h2>
               </div>
               <p className="text-sm text-[#4d6599] dark:text-gray-400 leading-relaxed">
--- src/components/core/landing-page/navbar/Navbar.tsx
+++ src/components/core/landing-page/navbar/Navbar.tsx
@@ -5,20 +5,14 @@ const Navbar = () => {
         <div className="max-w-7xl mx-auto flex items-center justify-between whitespace-nowrap">
           <div className="flex items-center gap-3">
             <div className="text-primary">
-              <svg
-                className="size-8"
-                fill="none"
-                viewBox="0 0 48 48"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  d="M42.1739 20.1739L27.8261 5.82609C29.1366 7.13663 28.3989 10.1876 26.2002 13.7654C24.8538 15.9564 22.9595 18.3449 20.6522 20.6522C18.3449 22.9595 15.9564 24.8538 13.7654 26.2002C10.1876 28.3989 7.13663 29.1366 5.82609 27.8261L20.1739 42.1739C21.4845 43.4845 24.5355 42.7467 28.1133 40.548C30.3042 39.2016 32.6927 37.3073 35 35C37.3073 32.6927 39.2016 30.3042 40.548 28.1133C42.7467 24.5355 43.4845 21.4845 42.1739 20.1739Z"
-                  fill="currentColor"
-                ></path>
-              </svg>
+              <img
+                src="/logicrow.png"
+                alt=""
+                className="objectcover h-12.5 w-12.5"
+              />
             </div>
             <h2 className="text-xl font-bold leading-tight tracking-tight dark:text-white">
-              LogisEscrow
+              Logicrow
             </h2>
           </div>
           <div className="hidden md:flex flex-1 justify-end gap-10 items-center">
